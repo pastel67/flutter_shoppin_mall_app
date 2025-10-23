f78a779d60dabfa0bfc0502e536e3a8053376581
pastel67 <chykjoun@gmail.com>
2025-10-24 08:55:03 +0900

    feat: 등록된 상품 없음 표시 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shoppin_mall_app/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       // theme: ThemeData(
       //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       // ),
-//      home: const MyHomePage(title: '쇼핑몰(가제)'),
+      home: const ShoppingHomePage(title: '쇼핑몰 앱(가제)'),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shoppin_mall_app/product_entity.dart';
 
-class ShoppingHomePage extends StatelessWidget{
+class ShoppingHomePage extends StatelessWidget {
+  const ShoppingHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  // List<ProductEntity> productList = [];
   @override
   Widget build(BuildContext context) {
-    return Text("ShoppingHomePage");
+    return Scaffold(
+      appBar: AppBar(title: Text(title, style: TextStyle(fontSize: 30))),
+      body: Center(child: Text("등록된 상품이 없습니다.")),
+    );
   }
 }
