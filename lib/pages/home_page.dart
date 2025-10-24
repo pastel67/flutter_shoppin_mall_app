f9fd37a051fa3c147828767974d68c62c9517c57
pastel67 <chykjoun@gmail.com>
2025-10-24 19:41:40 +0900

    커밋 실험

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -14,7 +14,7 @@ class ShoppingHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(title, style: TextStyle())),
-      body: Center(child: Text("등록된 상품이 없습니다.", style: TextStyle())),
+      body: Container(child: Center(child: Text("등록된 상품이 없습니다.", style: TextStyle()))),
     );
   }
 }
