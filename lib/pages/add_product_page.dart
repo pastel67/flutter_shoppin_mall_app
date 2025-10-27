7628a5c70f48f937df4fef73227e172745a7125d
pastel67 <chykjoun@gmail.com>
2025-10-27 10:54:31 +0900

    fix: 디테일 페이지

--- lib/pages/add_product_page.dart
+++ lib/pages/add_product_page.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
 class AddProductPage extends StatelessWidget {
+  String title;
+
+  AddProductPage(this.title);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("AddProductPage")));
+    return Scaffold(
+      appBar: AppBar(title: Text(title, style: TextStyle())),
+      body: Center(child: Text("AddProductPage")),
+    );
   }
 }
