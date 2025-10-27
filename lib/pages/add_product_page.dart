549a5606573ad4d5c2ca49ac6d74ffd381e45a7f
pastel67 <chykjoun@gmail.com>
2025-10-27 13:16:01 +0900

    feat: 상품 이름/상품 가격 입력란 하나의 메서드로 통합, ProductEntity에 동영상 링크 넣을 수 있는 변수 추가

--- lib/pages/add_product_page.dart
+++ lib/pages/add_product_page.dart
@@ -1,15 +1,64 @@
 import 'package:flutter/material.dart';
 
 class AddProductPage extends StatelessWidget {
-  String title;
-
   AddProductPage(this.title);
+
+  String title;
+  TextEditingController prodcutNameController = TextEditingController();
+
+  int productPrice = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(title, style: TextStyle())),
-      body: Center(child: Text("AddProductPage")),
+      body: Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(horizontal: 8),
+        child: Column(
+          children: [
+            Container(
+              color: Colors.amber.withValues(alpha: 0.3),
+              height: 200,
+              child: Center(child: Text("이미지")),
+            ),
+            SizedBox(height: 5),
+            inputField(title: "상품 이름"),
+            SizedBox(height: 5),
+            inputField(title: "상품 가격", isController: false, isFinction: true),
+            SizedBox(height: 5),            
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row inputField({
+    required String title,
+    bool isController = true,
+    bool isFinction = false,
+  }) {
+    return Row(
+      children: [
+        Text(title),
+        SizedBox(width: 10),
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(border: Border.all()),
+            child: TextField(
+              onChanged: (value) {
+                isFinction ? productPrice = int.tryParse(value) ?? 0 : null;
+              },
+              controller: isController ? prodcutNameController : null,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(left: 10),
+              ),
+              maxLines: 1,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
