c65a687791269c540441ff984c77bad8f6d76d6a
pastel67 <chykjoun@gmail.com>
2025-10-27 19:58:33 +0900

    fix: 텍스트 필드 입력 안되는 오류 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_shoppin_mall_app/pages/home_page.dart';
-import 'package:flutter_shoppin_mall_app/pages/description_page.dart';
 
 void main() {
   runApp(const MyApp());
